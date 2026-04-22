3e5cc75cd8129cc768479938770f1d0ab81daacd
Alex Silva <alexhctp@gmail.com>
2026-04-22 16:00:23 -0300

    Add script to fetch and rename CA certificates
    This script retrieves certificates from Wikipedia, renames them based on their subject CN, and updates the local CA certificates.

---
+++ ca-cert-utility.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+openssl s_client -showcerts -verify 5 -connect wikipedia.org:443 < /dev/null |
+   awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/{ if(/BEGIN CERTIFICATE/){a++}; out="cert"a".pem"; print >out}'
+for cert in *.pem; do 
+        newname=$(openssl x509 -noout -subject -in $cert | sed -nE 's/.*CN ?= ?(.*)/\1/; s/[ ,.*]/_/g; s/__/_/g; s/_-_/-/; s/^_//g;p' | tr '[:upper:]' '[:lower:]').crt
+        echo "${newname}"; mv "${cert}" "${newname}" 
+done
+
+sudo mv *.crt /usr/local/share/ca-certificates
+sudo update-ca-certificates
